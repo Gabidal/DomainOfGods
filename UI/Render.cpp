2c9fa1bdd4c692c1cb8118cfc3f918e8a677337f
Gabidal <golzar.gabriel@gmail.com>
2023-04-22 15:08:03 +0300

    .

--- UI/Render.cpp
+++ UI/Render.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 namespace RENDER{
     unordered_map<int, Texture2D> Tile_Textures;
+    unordered_map<SPECIES, Texture2D> Entity_Textures;
 
     void Init(){
         // Do some environment vibe cheks.
@@ -22,9 +23,16 @@ namespace RENDER{
         InitWindow(GLOBALS::WINDOW_HEIGHT, GLOBALS::WINDOW_WIDTH, "Domain Of Gods");
 
         Tile_Textures = {
-            {0, LoadTexture("Img/Grass.png")},       // Normal grass
-            {1, LoadTexture("Img/Wet_Grass.png")},   // Wet grass
-            {2, LoadTexture("Img/Swamp_Grass.png")}  // Swamp grass
+            {0, LoadTexture("Img/Tile/Grass.png")},       // Normal grass
+            {1, LoadTexture("Img/Tile/Wet_Grass.png")},   // Wet grass
+            {2, LoadTexture("Img/Tile/Swamp_Grass.png")}  // Swamp grass
+        };
+
+        Entity_Textures = {
+            {SPECIES::HUMAN, LoadTexture("Img/Entity/Human.png")},
+            {SPECIES::GOBLIN, LoadTexture("Img/Entity/Goblin.png")},
+            {SPECIES::ELF, LoadTexture("Img/Entity/Elf.png")},
+
         };
 
         GLOBALS::CAMERA.Y = GLOBALS::CHUNK_HEIGHT / 2;
@@ -35,6 +43,25 @@ namespace RENDER{
         // end all
 
         CloseWindow();
+    }
+
+    FVector3 Get_Relative_Position_To_Camera(Location position, Texture2D texture){
+        int Tile_Width = texture.width;
+        int Tile_Height = texture.height;
+
+        int Tile_Position_Y = position.HIGH.Y * Tile_Height;
+        int Tile_Position_X = position.HIGH.X * Tile_Width;
+
+        int Chunk_Position_Y = position.CHUNK.Y * GLOBALS::CHUNK_HEIGHT * Tile_Height;
+        int Chunk_Position_X = position.CHUNK.X * GLOBALS::CHUNK_WIDTH * Tile_Width;
+
+        int Camera_Position_Y = GLOBALS::CAMERA.Y * GLOBALS::CHUNK_HEIGHT * Tile_Height;
+        int Camera_Position_X = GLOBALS::CAMERA.X * GLOBALS::CHUNK_WIDTH * Tile_Width;
+
+        float Relative_Y = Tile_Position_Y + Chunk_Position_Y - Camera_Position_Y;
+        float Relative_X = Tile_Position_X + Chunk_Position_X - Camera_Position_X;
+
+        return FVector3{Relative_X, Relative_Y, 0.f};
     }
 
     void Update_Frame(){
@@ -48,22 +75,17 @@ namespace RENDER{
 
         // First draw the background with tilesseses-
         for (auto* tile : MAP::Get_Surrounding_Content(GLOBALS::CAMERA, 5)){
-            int Tile_Width = Tile_Textures[tile->ID].width;
-            int Tile_Height = Tile_Textures[tile->ID].height;
+            FVector3 World_Space = Get_Relative_Position_To_Camera(tile->Position, Tile_Textures[tile->ID]);
 
-            int Tile_Position_Y = tile->Position.HIGH.Y * Tile_Height;
-            int Tile_Position_X = tile->Position.HIGH.X * Tile_Width;
+            DrawTexture(Tile_Textures[tile->ID], World_Space.X, World_Space.Y, WHITE);
+        }
 
-            int Chunk_Position_Y = tile->Position.CHUNK.Y * GLOBALS::CHUNK_HEIGHT * Tile_Height;
-            int Chunk_Position_X = tile->Position.CHUNK.X * GLOBALS::CHUNK_WIDTH * Tile_Width;
+        // now draw the entities
+        for (auto* entity : CHAOS::Get_Surrounding_Content(GLOBALS::CAMERA, 5)){
+            FVector3 World_Space = Get_Relative_Position_To_Camera(entity->Get_Position(), Entity_Textures[entity->Get_Specie()->Specie]);
 
-            int Camera_Position_Y = GLOBALS::CAMERA.Y * GLOBALS::CHUNK_HEIGHT * Tile_Height;
-            int Camera_Position_X = GLOBALS::CAMERA.X * GLOBALS::CHUNK_WIDTH * Tile_Width;
-
-            int Relative_Y = Tile_Position_Y + Chunk_Position_Y - Camera_Position_Y;
-            int Relative_X = Tile_Position_X + Chunk_Position_X - Camera_Position_X;
-
-            DrawTexture(Tile_Textures[tile->ID], Relative_X, Relative_Y, WHITE);
+            // Use directional mappings.
+            DrawTexture(Entity_Textures[entity->Get_Specie()->Specie], World_Space.X, World_Space.Y, WHITE);
         }
 
         EndDrawing();
